c43f7f2064ee71222664f83304003ca493d28614
erdogan98 <ekervanli@outook.com>
2022-08-09 12:26:24 +0100

    deleted cholesky operator from wrong location

--- ivy/functional/frontends/jax/lax/operators.py
+++ ivy/functional/frontends/jax/lax/operators.py
@@ -19,9 +19,6 @@ tan.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
 def concatenate(operands, dimension):
     return ivy.concat(operands, dimension)
 
-def cholesky(x:[...,n,n], *, symmetrize_input=True):
-    return ivy.cholesky(x, symmetrize_input=symmetrize_input)
-
 def full(shape, fill_value, dtype=None):
     return ivy.full(shape, fill_value, dtype=dtype)
 
